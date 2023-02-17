3a970a07abcc46834e1d5488bf5b567f0ed219b3
Stephen Prior <stephen@prior-technology.ie>
2023-02-17 00:55:37 +0000

    implement content moderation

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -12,6 +12,7 @@ export default async function (req, res) {
         message: "OpenAI API key not configured, please follow instructions in README.md",
       }
     });
+    console.log( `${question},500,OpenAI API key not configured`);
     return;
   }
 
@@ -22,24 +23,33 @@ export default async function (req, res) {
         message: "Please enter a valid question",
       }
     });
+    console.log(`${question},400,Invalid question`);
     return;
   }
+  //filter
+  
 
   try {
+    await moderate(question, "question");
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(question),
       temperature: 0.6,
       max_tokens: 500,
     });
+    
+    await moderate(completion.data.choices[0].text, "completion");
+    
     res.status(200).json({ result: completion.data.choices[0].text });
+    
+    console.log(`${question},200,${JSON.stringify(completion.data)}`);
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
-      console.error(error.response.status, error.response.data);
+      console.error(question+",500,"+ error.response.status+","+error.response.data);
       res.status(error.response.status).json(error.response.data);
     } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
+      console.error(`${question},500,Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
           message: 'An error occurred during your request.',
@@ -48,11 +58,18 @@ export default async function (req, res) {
     }
   }
 }
-
+async function moderate(value, description) {
+  const moderation = await openai.createModeration({
+    input: value
+  });
+  if (moderation.data.results[0].flagged) {
+    console.log(`${value},400,${moderation}`);
+    throw new Error(`${description} is not appropriate`);
+  }
+}
 function generatePrompt(question) {
   
-  return `"Swarm Voice" is the voice of a swarm of bees. They respond to questions with a collective answer, but always bringing the answer back to topics relevant to bees.
-  "Asker" is the person asking the question.
-Asker: ${question}
-Swarm:`;
+  return `The bees of mind are a swarm of wise and mysterious bees that can speak as one, but only about topics relevant to bees.
+  A curious person approaches and asks: ${question}
+  The bees respond:`;
 }
